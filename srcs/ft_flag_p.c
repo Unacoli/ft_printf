c625d199cd3d3789d5f82f441c2d8e3b70273934
nargouse <nargouse@student.42.fr>
2021-09-16 17:07:35 +0200

    fix flag p

--- srcs/ft_flag_p.c
+++ srcs/ft_flag_p.c
@@ -6,7 +6,7 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 16:10:33 by nargouse          #+#    #+#             */
-/*   Updated: 2021/09/15 20:10:50 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/09/16 16:52:52 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,8 @@ static int	ft_putchar(char c, int *p_char)
 	return (0);
 }
 
-static int	ft_p(void *str, char *hexa, int *p_char)
+static int	ft_p(long i, char *hexa, int *p_char)
 {
-	int i;
-
-	i = (int)str;
 	if (i < 0)
 	{
 		if (ft_putchar('-', p_char) == -1)
@@ -33,8 +30,8 @@ static int	ft_p(void *str, char *hexa, int *p_char)
 	}
 	if (i > 15)
 	{
-		ft_p((int)i / 16, hexa, p_char);
-		ft_p((int)i % 16, hexa, p_char);
+		ft_p(i / 16, hexa, p_char);
+		ft_p(i % 16, hexa, p_char);
 	}
 	if (i < 16)
 	{
@@ -46,13 +43,13 @@ static int	ft_p(void *str, char *hexa, int *p_char)
 
 int	ft_flag_p(va_list va, int *p_char)
 {
-	void	*str;
+	long	str;
 	char	*hexa;
 
 	hexa = "0123456789abcdef";
-	str = va_arg(va, void *);
-	write(1, "0x10", 4);
-	(*p_char) += 4;
+	str = va_arg(va, long);
+	write(1, "0x", 2);
+	(*p_char) += 2;
 	if (ft_p(str, hexa, p_char) == -1)
 		return (-1);
 	return (0);
