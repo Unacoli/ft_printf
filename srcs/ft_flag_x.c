da9d2088dc28f4117e529f80db769838be05e262
nargouse <nargouse@student.42.fr>
2021-09-16 17:46:50 +0200

    fix some norm

--- srcs/ft_flag_x.c
+++ srcs/ft_flag_x.c
@@ -6,13 +6,13 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 16:12:45 by nargouse          #+#    #+#             */
-/*   Updated: 2021/09/15 20:00:32 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/09/16 17:14:23 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static int ft_putchar(char c, int *p_char)
+static int	ft_putchar(char c, int *p_char)
 {
 	if (write(1, &c, 1) == -1)
 		return (-1);
@@ -20,17 +20,11 @@ static int ft_putchar(char c, int *p_char)
 	return (0);
 }
 
-static int	ft_x(int n, int *p_char)
+static int	ft_x(unsigned int n, int *p_char)
 {
 	char	*hexa;
 
 	hexa = "0123456789abcdef";
-	if (n < 0)
-	{
-		if (ft_putchar('-', p_char) == -1)
-			return (-1);
-		n *= -1;
-	}
 	if (n > 15)
 	{
 		ft_x(n / 16, p_char);
@@ -44,17 +38,11 @@ static int	ft_x(int n, int *p_char)
 	return (0);
 }
 
-static int	ft_X(int n, int *p_char)
+static int	ft_X(unsigned int n, int *p_char)
 {
 	char	*hexa;
 
 	hexa = "0123456789ABCDEF";
-	if (n < 0)
-	{
-		if (ft_putchar('-', p_char) == -1)
-			return (-1);
-		n *= -1;
-	}
 	if (n > 15)
 	{
 		ft_X(n / 16, p_char);
@@ -70,9 +58,9 @@ static int	ft_X(int n, int *p_char)
 
 int	ft_flag_x(va_list va, int *p_char, char format)
 {
-	int	n;
+    unsigned int	n;
 
-	n = va_arg(va, int);
+	n = va_arg(va, unsigned int);
 	if (format == 'x')
 	{
 		if (ft_x(n, p_char) == -1)
