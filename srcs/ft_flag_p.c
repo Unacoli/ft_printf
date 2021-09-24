9883faad0a68464d69c2be579755a69517dec572
nargouse <nargouse@student.42.fr>
2021-09-24 18:10:30 +0200

    fix ulong_max

--- srcs/ft_flag_p.c
+++ srcs/ft_flag_p.c
@@ -6,7 +6,7 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 16:10:33 by nargouse          #+#    #+#             */
-/*   Updated: 2021/09/24 17:56:45 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/09/24 18:10:09 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,18 @@ static int	ft_long_abs(long i, int *p_char)
 	{
 		if (write(1, "8000000000000000", 16) == -1)
 			return (-1);
-		(*p_char) += 16;
 	}
 	else if (i == LONG_MAX)
 	{
 		if (write(1, "7fffffffffffffff", 16) == -1)
 			return (-1);
-		(*p_char) += 16;
 	}
+	else if ((unsigned long)i == ULONG_MAX)
+	{
+		if (write(1, "ffffffffffffffff", 16) == -1)
+			return (-1);
+	}
+	(*p_char) += 16;
 	return (0);
 }
 
@@ -39,7 +43,7 @@ static int	ft_putchar(char c, int *p_char)
 
 static int	ft_p(long i, char *hexa, int *p_char)
 {
-	if (i == LONG_MIN || i == LONG_MAX)
+	if (i == LONG_MIN || i == LONG_MAX || (unsigned long)i == ULONG_MAX)
 	{
 		if (ft_long_abs(i, p_char) == -1)
 			return (-1);
